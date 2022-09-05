1df171746e9cb37fc17a59b1457408c481b432c5
overun <yankr555@gmail.com>
2022-09-05 23:46:05 +0900

    lint ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { client } from "../libs/client";
 import { Article } from "../types/article";
 import Link from "next/link";
@@ -15,24 +16,19 @@ export default function Home({ articles }: Props) {
       <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
         {articles.map((article) => (
           <Link href={`/article/${article.id}`} passHref>
-            <a>
-              <div
-                className="rounded overflow-hidden shadow-lg"
-                key={article.id}
-              >
-                <img
-                  className="w-full"
-                  src={article.eye_catch.url}
-                  alt="Sunset in the mountains"
-                />
-                <div className="px-6 py-4">{article.title}</div>
-                <div className="px-6 pt-4 pb-2">
-                  {article.tag && (
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #{article.tag}
-                    </span>
-                  )}
-                </div>
+            <a className="rounded overflow-hidden shadow-lg" key={article.id}>
+              <img
+                className="w-full"
+                src={article.eye_catch.url}
+                alt="Sunset in the mountains"
+              />
+              <div className="px-6 py-4">{article.title}</div>
+              <div className="px-6 pt-4 pb-2">
+                {article.tag && (
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #{article.tag}
+                  </span>
+                )}
               </div>
             </a>
           </Link>
