0fa9a0f1bcfa77232fa897b670c3eab557c421e2
overun <yankr555@gmail.com>
2022-09-09 12:07:06 +0900

    clientチェック

--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN || "",
-  apiKey: process.env.API_KEY || "",
+  serviceDomain: "blogtry",
+  apiKey: "f16c837ef86e4219b28c31461811ffb40b2c",
 });
